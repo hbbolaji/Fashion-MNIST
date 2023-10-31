afe44042f67ba3b3b58b6aab547a4481ee2a6560
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2023-10-31 09:30:25 +0300

    image dispaly function

--- helper_function.py
+++ helper_function.py
@@ -1,7 +1,11 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 from test_step import test_step
+
+# Hyperparameters
+classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 def accuracy(logits:torch.Tensor, labels:torch.Tensor):
   prediction = torch.argmax(logits, dim=1)
@@ -19,5 +23,9 @@ def eval_model(model:nn.Module, data_loader: DataLoader, criterion: nn.Module, a
       "model_accuracy": acc
     }
 
-def visualize():
+def visualize(images:torch.Tensor, labels:torch.Tensor):
+  for i in range(10):
+    plt.subplot(2, 5, i+1)
+    plt.imshow(images[i].view(28, 28, 1), cmap='gray')
+    plt.title(classes[labels[i]])
   return
--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import torch.nn as nn
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 import os.path
+import matplotlib.pyplot as plt
 from FashionMNISTModel import FashionMNISTModel
 from train_step import train_step
 from test_step import test_step
-from helper_function import accuracy, eval_model
+from helper_function import accuracy, eval_model, visualize
 
 # Hyperparameter
-epochs = 1
+epochs = 10
 lr = 0.01
 batch_size = 32
-exploration = False
+exploration = False  # Change to True to see display 6 random train images and False to train the model
 
 # Device agnostic
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -26,34 +27,36 @@ test_set = datasets.FashionMNIST('./data', train=False, download=True, transform
 
 classes = train_set.classes
 class_to_idx = train_set.class_to_idx
-print(classes, class_to_idx)
+
 # dataloader
 train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=batch_size)
 
-# Model
-torch.manual_seed(42)
-model = FashionMNISTModel(input_shape= 28*28,
-                          hidden_units=10,
-                          output_shape=10).to(device)
+if exploration:
+  samples = iter(train_loader)
+  images, labels = next(samples)
+  visualize(images, labels)
+  plt.show()
+else:
+  # Model
+  torch.manual_seed(42)
+  model = FashionMNISTModel(input_shape= 28*28,
+                            hidden_units=10,
+                            output_shape=10).to(device)
 
-# Optimizer and loss Function
-criterion = nn.CrossEntropyLoss()
-# optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+  # Optimizer and loss Function
+  criterion = nn.CrossEntropyLoss()
+  optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
-# Training and Testing Loop
-# for epoch in range(epochs):
-#   print(f'{epoch + 1} / {epochs}')
-#   train_step(model=model, data_loader=train_loader, criterion=criterion, optimizer=optimizer, accuracy=accuracy, device=device)
-#   test_step(model, test_loader, criterion, accuracy, device)
+  # Training and Testing Loop
+  for epoch in range(epochs):
+    print(f'{epoch + 1} / {epochs}')
+    train_step(model=model, data_loader=train_loader, criterion=criterion, optimizer=optimizer, accuracy=accuracy, device=device)
+    test_step(model, test_loader, criterion, accuracy, device)
 
-# # Model Evaluation
-# model_result = eval_model(model, test_loader, criterion, accuracy)
-# print(model_result)
-
-
-
-
+  # Model Evaluation
+  model_result = eval_model(model, test_loader, criterion, accuracy)
+  print(model_result)
 
 # saving model
 if os.path.isfile('./model.pth'):
